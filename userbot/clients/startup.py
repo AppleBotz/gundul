09f05228a647c81e9830a1b996852a23e250657b
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-02-15 12:55:14 +0700

    Update startup.py

--- userbot/clients/startup.py
+++ userbot/clients/startup.py
@@ -29,8 +29,8 @@ from userbot import (
 )
 from userbot.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nMan-UserBot v{}, Copyright © 2021-2022 ʀɪsᴍᴀɴ• <https://github.com/mrismanaziz>"
-MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nMan-UserBot v{}, Copyright © 2021-2022 ʀɪsᴍᴀɴ• <https://github.com/mrismanaziz>"
+EOL = "EOL\nBlvckcards v{}, Copyright © 2021-2022 ʀɪsᴍᴀɴ• <https://github.com/mrismanaziz>>"
+MSG_BLACKLIST = "Sorry Your Account Has Been Turn Off , USERBOT {} Non Activaed.\nBlvck-UserBot v{}, Copyright © 2021-2022 ʀɪsᴍᴀɴ• <https://github.com/mrismanaziz>>
 
 
 async def man_client(client):
