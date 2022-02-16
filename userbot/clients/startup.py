dd8ca25da841e90c9e5b147bfef59a513a9286d2
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-02-16 10:20:06 +0700

    Update startup.py

--- userbot/clients/startup.py
+++ userbot/clients/startup.py
@@ -29,8 +29,8 @@ from userbot import (
 )
 from userbot.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nBlvckcards v{}, Copyright © 2021-2022 Matrix • <https://github.com/mrismanaziz>>"
-MSG_BLACKLIST = "Sorry Your Account Has Been Turn Off , USERBOT {} Non Activaed.\nBlvck-UserBot v{}, Copyright © 2021-2022 Matrix • <https://github.com/mrismanaziz>>"
+EOL = "EOL\nBlvckcards v{}, Copyright © 2021-2022 Matrix • <https://github.com/Matrix>"
+MSG_BLACKLIST = "Sorry Your Account Has Been Turn Off , USERBOT {} Non Activaed.\nBlvck-UserBot v{}, Copyright © 2021-2022 Matrix • <https://github.com/Matrix>"
 
 
 async def man_client(client):
@@ -39,13 +39,13 @@ async def man_client(client):
 
 
 def multiman():
-    if 844432220 not in DEVS:
+    if 5251274635 not in DEVS:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     if -1001473548283 not in GBL:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
-    if 844432220 not in DEFAULT:
+    if 5251274635 not in DEFAULT:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     failed = 0
